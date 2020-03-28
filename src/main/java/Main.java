907d895f57b8378c7bf5829f4d1e9b9a13403ce0
Rafał Załęski <rafalzaleski@gmail.com>
2020-03-28 10:35:45 +0100

    tasks, select from user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,17 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/shop?serverTimezone=CET";
         String user = "root";
         String password = "K0czk0d4n!";
-        String query = "SELECT * from country;";
+        String query = "SELECT * from user;";
 
         try (Connection connection = DriverManager.getConnection(url, user, password);
              Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
-                System.out.println("Country ID: " + resultSet.getInt("CO_ID"));
-                System.out.println("Country Name: " + resultSet.getString("CO_NAME"));
-                System.out.println("Country Alias: " + resultSet.getString("CO_ALIAS"));
+//                System.out.println("Country ID: " + resultSet.getInt("CO_ID"));
+//                System.out.println("Country Name: " + resultSet.getString("CO_NAME"));
+//                System.out.println("Country Alias: " + resultSet.getString("CO_ALIAS"));
+                System.out.println("Imię: " + resultSet.getString("USR_FIRSTNAME"));
+                System.out.println("Nazwisko: " + resultSet.getString("USR_LASTNAME"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
