2ea64076d6c9bc7eab74f04f351ce116f46d092b
Rafał Załęski <rafalzaleski@gmail.com>
2020-03-28 10:23:03 +0100

    first steps in jdbc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,37 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
-        String url = "jdbc.mysql://localhost:3306/shop";
-        connection = DriverManager.getConnection()
+        String url = "jdbc:mysql://localhost:3306/shop?serverTimezone=CET";
+        String user = "root";
+        String password = "K0czk0d4n!";
+
+        try {
+            String query = "SELECT * from country;";
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("CO_ID"));
+                System.out.println(resultSet.getString("CO_NAME"));
+                System.out.println(resultSet.getString("CO_ALIAS"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
