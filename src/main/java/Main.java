56c8756fc2037ff14ac6270437d489b7f4982c84
Rafał Załęski <rafalzaleski@gmail.com>
2020-03-28 09:53:29 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        String url = "jdbc.mysql://localhost:3306/shop";
+        connection = DriverManager.getConnection()
 
     }
 }
